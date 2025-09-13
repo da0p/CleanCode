75ffb531970a74ff5ffb3f858f6f569639da4c6e
Phuong Dao <phuong018@gmail.com>
2025-09-13 10:44:31 +0700

    Add .clang-format

--- example/PizzaStore.cpp
+++ example/PizzaStore.cpp
@@ -1,67 +1,63 @@
 #include "PizzaStore.h"
 
 namespace ThisIsAPizzaStoreNamespace {
- void
- PizzaStore::orderPizza(const std::string& pizzaName) {
+void PizzaStore::orderPizza(const std::string& pizzaName) {
   // check if the order is valid
-  if (pizzaName != "Margherita" && pizzaName != "Hawaiian" && pizzaName != "Napoletan" && pizzaName != "Marinara" && pizzaName != "Ortolana") {
-   return;
+  if (pizzaName != "Margherita" && pizzaName != "Hawaiian" &&
+      pizzaName != "Napoletan" && pizzaName != "Marinara" &&
+      pizzaName != "Ortolana") {
+    return;
   }
-  
+
   // accept the order
   p.push_back(pizzaName);
- }
- 
- void
- PizzaStore::makePizza() {
+}
+
+void PizzaStore::makePizza() {
   for (const auto& n : p) {
-   std::string topping = t.count(n) ? " with topping " + t.at(n) : "";
-   std::cout << "Doing " << n << " pizza" << topping << "\n";
+    std::string topping = t.count(n) ? " with topping " + t.at(n) : "";
+    std::cout << "Doing " << n << " pizza" << topping << "\n";
   }
- }
- 
- void PizzaStore::tiktok() {
+}
+
+void PizzaStore::tiktok() {
   for (const auto& n : p) {
-   std::cout << "Showing pizza " << n << " on tiktok\n";
+    std::cout << "Showing pizza " << n << " on tiktok\n";
   }
- }
- 
- void
- PizzaStore::orderDrink(const std::string& drinkName) {
-  if (drinkName != "Vodka" && drinkName != "Whiskey" && drinkName != "Rice Wine" && drinkName != "Coke") {
-   return;
+}
+
+void PizzaStore::orderDrink(const std::string& drinkName) {
+  if (drinkName != "Vodka" && drinkName != "Whiskey" &&
+      drinkName != "Rice Wine" && drinkName != "Coke") {
+    return;
   }
-  
+
   d.push_back(drinkName);
- }
- 
- void
- PizzaStore::makeDrink() {
+}
+
+void PizzaStore::makeDrink() {
   for (const auto& r : d) {
-   std::cout << "Making drink " << r << "\n";
+    std::cout << "Making drink " << r << "\n";
   }
- }
- 
- void
- PizzaStore::addTopping(const std::string& pizza_name, const std::string& topping_name) {
+}
+
+void PizzaStore::addTopping(const std::string& pizza_name,
+                            const std::string& topping_name) {
   t[pizza_name] = topping_name;
- }
- 
- void
- PizzaStore::showAllOrders() {
+}
+
+void PizzaStore::showAllOrders() {
   for (const auto& pizza : p) {
     std::cout << "Pizza order:\n";
     std::cout << "Name: " << pizza << "\n";
     if (t.count(pizza)) {
-     std::cout << "Topping: " << t.at(pizza) << "\n";
+      std::cout << "Topping: " << t.at(pizza) << "\n";
     }
   }
- }
- 
- void
- PizzaStore::partyHard() {
-   // This is cute and I like it!
-   std::cout << "order everything!\n";
- }
 }
- 
+
+void PizzaStore::partyHard() {
+  // This is cute and I like it!
+  std::cout << "order everything!\n";
+}
+}  // namespace ThisIsAPizzaStoreNamespace
--- example/PizzaStore.h
+++ example/PizzaStore.h
@@ -9,22 +9,25 @@
 namespace ThisIsAPizzaStoreNamespace {
 
 class PizzaStore {
-public:
-PizzaStore() = default;
-    void tiktok();
-        void makePizza();
-            void orderPizza(const std::string& pizzaName);
-                void deliver();
-                    void addTopping(const std::string& pizza_name, const std::string& topping_name);
-                void removeTopping(const std::string& pizza_name, const std::string& topping_name);
-            void orderDrink(const std::string& drink_name);
-        void makeDrink();
-    void showAllOrders();
-    void partyHard();
-private:
-        std::vector<std::string> p; // all pizza orders
-    std::vector<std::string> d; // all drink orders
-    std::unordered_map<std::string, std::string> t; // all topping 
+ public:
+  PizzaStore() = default;
+  void tiktok();
+  void makePizza();
+  void orderPizza(const std::string& pizzaName);
+  void deliver();
+  void addTopping(const std::string& pizza_name,
+                  const std::string& topping_name);
+  void removeTopping(const std::string& pizza_name,
+                     const std::string& topping_name);
+  void orderDrink(const std::string& drink_name);
+  void makeDrink();
+  void showAllOrders();
+  void partyHard();
+
+ private:
+  std::vector<std::string> p;                      // all pizza orders
+  std::vector<std::string> d;                      // all drink orders
+  std::unordered_map<std::string, std::string> t;  // all topping
 };
-}
+}  // namespace ThisIsAPizzaStoreNamespace
 #endif
