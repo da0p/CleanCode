1c13ba37426d8326ad3c12ff8d9c4851359e62f1
Phuong Dao <phuong018@gmail.com>
2025-09-13 11:31:06 +0700

    Add .clang-tidy

--- example/main.cpp
+++ example/main.cpp
@@ -1,17 +1,17 @@
 #include "PizzaStore.h"
 
 int main() {
-    ThisIsAPizzaStoreNamespace::PizzaStore store;
-    store.orderPizza("Margherita");
-    store.addTopping("Margherita", "Basil");
-    store.orderPizza("Hawaiian");
-    store.addTopping("Hawaiian", "Pineapple");
-    store.orderDrink("Coke");
-    store.orderDrink("Vodka");
-    store.makePizza();
-    store.makeDrink();
-    store.tiktok();
-    store.showAllOrders();
-    store.partyHard();
-    return 0;
+  ThisIsAPizzaStoreNamespace::PizzaStore store;
+  store.orderPizza("Margherita");
+  store.addTopping("Margherita", "Basil");
+  store.orderPizza("Hawaiian");
+  store.addTopping("Hawaiian", "Pineapple");
+  store.orderDrink("Coke");
+  store.orderDrink("Vodka");
+  store.makePizza();
+  store.makeDrink();
+  store.tiktok();
+  store.showAllOrders();
+  store.partyHard();
+  return 0;
 }
